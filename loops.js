057af8d2082ab1ea07d4de55e6daa0ed4c95901f
roshancs12 <roshanvaniyan322@gmail.com>
2026-09-15 16:58:14 +0530

    Added JavaScript loops

---
+++ loops.js
@@ -0,0 +1,80 @@
+
+
+// for(let i=1;i<=10000;i++){
+//   console.log("roshan");
+  
+// }
+// console.log("loops are ended");
+
+
+// var sum=0;
+// for(i=1;i<=5;i++){
+//   sum=sum+i;
+//   console.log("sum=",sum);
+// }
+// console.log("sum=",sum);
+
+// var i=1;
+// while(i<=5){
+//   console.log(i);
+//   i++;
+// }
+
+// //sum of 1 to 100
+// let sum=0;
+// let n=100;
+// for(let i=1;i<=n;i++){
+//   sum=sum+i;
+
+
+// }
+// console.log("sum=",sum);
+
+// console.log("loops are ended here");
+
+// let i=1;
+// do{
+//   console.log("i=",i);
+//   i++;
+//     console.log("Apna college");
+// }while(i<=10)
+
+//for-of loops
+
+// let str=prompt("Enter a value");
+
+// for(let i of str){
+//   console.log(i);
+// }
+
+
+// let student={
+//   name:"roshan",
+//   age:24,
+//   address:"palla",
+//   pin:111
+// }
+
+// for(let key in student){
+//   console.log("key",key,"values=",student[key]);
+// }
+
+//print even number 0 to 100 (p1)
+//  let num=prompt("enter a number");
+//  for(i=0;i<=num;i++){
+//   if(i%2==0){
+//     console.log("even number",i);
+//   }
+//   console.log("odd number",i);
+//  }
+
+//p2
+
+let gamenum=10;
+
+let num=prompt("enter a number");
+
+while(gamenum!=num){
+  console.log("you entered wrong number plz again enter");
+}
+console.log("you have guessed correct number");
