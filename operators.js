6cc5e7afb47d19db9d92ccbdfbcbd02a7f9a6fbe
roshancs12 <roshanvaniyan322@gmail.com>
2026-09-15 17:03:41 +0530

    Added JavaScript operators

--- operators.js
+++ operators.js
@@ -1,4 +1,4 @@
-/*var a=10;
+var a=10;
 var b=20;
 
 console.log("a+b",a+b);
@@ -12,10 +12,10 @@ console.log("a**b",a**b);//a^b*/
 
 //unary operator
 
-/*let a=40;
+let a=40;
 let b=30;
 
-/*a=a+b;
+a=a+b;
 console.log(a);
 
 a=a-b;
@@ -26,19 +26,19 @@ console.log(a++);
 console.log(a);
 
 console.log(++a);
-console.log(a); */
+console.log(a); 
 
 
-/*console.log(a--);
+console.log(a--);
 console.log(a);
 
 console.log(--a);
-console.log(a); */
+console.log(a); 
 
 
 //assignent operator
 
-/*let a=20;
+let a=20;
 
 a+=1//a=a+1
 
@@ -46,18 +46,18 @@ console.log(a);
 
 let b=4;
 b-=2;//b=b+2
-console.log(b);*/
+console.log(b);
 
 //COMPARISON operator
 
-/*let a=4;
+let a=4;
 let b=4;
 
 console.log("a==b",a==b);
 
 console.log("a!=b",a!==b);
 
-console.log("a===b",a===b);*/
+console.log("a===b",a===b);
 
 
 let a=4;
@@ -75,10 +75,4 @@ let b=3;
 
   console.log("opt1 ! opt2=", !a<b);
 
-    console.log("opt1 ! opt2=", !a===4);
-
-
-  
-
-
-
+    console.log("opt1 ! opt2=", !a===4)
