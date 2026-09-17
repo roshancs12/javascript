1e9a6981d2e55d6a4f869536cf9fe9435b8362b1
roshancs12 <roshanvaniyan322@gmail.com>
2026-09-17 16:57:58 +0530

    added js functions

---
+++ function.js
@@ -0,0 +1,20 @@
+//function-block of code that perform a specific task cant be invoked whenever we needed
+
+
+
+// function myfirstfunction(msg) {
+//   console.log("roshan");
+//   console.log("im learning js");
+
+// }
+// myfirstfunction()
+
+function sum(x,y){
+  s=x+y;
+  console.log(x)
+  return s;
+  
+}
+
+let val=sum(8,5)
+console.log(val);
