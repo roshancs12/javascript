d8c309473d7485fe203774a8bc7e07239f53b501
roshancs12 <roshanvaniyan322@gmail.com>
2026-09-17 16:56:14 +0530

    updated code

--- array.js
+++ array.js
@@ -1,28 +1,56 @@
-//array is coolection of items
+// //array is coolection of items
 
 
-items=["item1","items2","item3","item4"]
+// items=["item1","items2","item3","item4"]
 
-for(ind=0;ind<=items.length;ind++){
- console.log(items[ind]);
-}
+// for(ind=0;ind<=items.length;ind++){
+//  console.log(items[ind]);
+// }
 
 
-for(let item of items){
-  console.log(item);
-}
+// for(let item of items){
+//   console.log(item);
+// }
  
 
 
-cities=["bangalore","kochi","mangalore","mumbai","chennai"]
-for(let city of cities){
-  console.log(city);
-}
+// cities=["bangalore","kochi","mangalore","mumbai","chennai"]
+// for(let city of cities){
+//   console.log(city);
+// }
 
-let marks=[23,45,67,67,67]
-let sum=0;
-for(let val of marks){
-  sum=sum+val;
-}
-let avg=sum/marks.length
-console.log(avg);
+// let marks=[23,45,67,67,67]
+// let sum=0;
+// for(let val of marks){
+//   sum=sum+val;
+// }
+// let avg=sum/marks.length
+// console.log(avg);
+
+
+// let name=['rosha','vishnu','gawtham','vaibhav'];
+
+// name.push("sharath");
+// console.log(name);
+
+// name.pop("sharath");
+
+// console.log(name);
+// name.toString(name);
+// console.log(name);
+
+// let veg=["tomato","potato","bettrot","chilly"];
+// let fruit=["pineapple","orange","jackfruit","grapes"];
+
+// let both=veg.concat(fruit);
+// console.log(both);
+
+
+// console.log(veg);
+// console.log(veg.slice());
+
+
+let arr=[1,2,3,4,5,6,7];
+arr.splice(2,2,101,102);
+
+arr.splice(4);
