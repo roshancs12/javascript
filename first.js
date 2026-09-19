e7fb45bd3f63bcd70b25ef6652f3b838b6f1f051
roshancs12 <roshanvaniyan322@gmail.com>
2026-09-19 16:08:59 +0530

    basics.js

--- first.js
+++ first.js
@@ -1,7 +1,23 @@
 //console.log- used to print msg to the console
 
-console.log("apna college ");
-console.log("apna college ");
-console.log("roshan ");
-console.log("i lovejs ");
+// console.log("apna college ");
+// console.log("apna college ");
+// console.log("roshan ");
+// console.log("i lovejs ");
 
+
+
+let a="123rosha";
+console.log(a);
+let b=parseInt(a)
+console.log(b);
+
+
+let num=123;
+console.log(num);
+let str=String(num)
+console.log(str);
+
+let num2=1;
+console.log(num2);
+console.log(Boolean(num2));``
